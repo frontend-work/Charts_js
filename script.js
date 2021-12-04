e8cfc4c8d473b749b1dbae8a967c74ae4257153b
Luminita Robciuc <88823221+frontend-work@users.noreply.github.com>
2021-12-04 16:21:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 let labels1 = ['YES', 'YES BUT IN GREEN'];
 let data1 = [69, 31];
 let colors1 = ['#49A9EA', '#36CAAB'];
-
 let myDoughnutChart = document.getElementById("myChart1").getContext('2d');
-
 let chart1 = new Chart(myDoughnutChart, {
     type: 'doughnut',
     data: {
@@ -22,14 +20,10 @@ let chart1 = new Chart(myDoughnutChart, {
     }
 });
 
-
-
 let labels2 = ['American Airlines Group', 'Ryanair', 'China Southern Airlines', 'Lufthansa Group'];
 let data2 = [199.6, 130.3, 126.3, 130];
 let colors2 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF'];
-
 let myChart2 = document.getElementById("myChart2").getContext('2d');
-
 let chart2 = new Chart(myChart2, {
     type: 'bar',
     data: {
@@ -50,11 +44,8 @@ let chart2 = new Chart(myChart2, {
     }
 });
 
-
-
 let labels3 = ['Attack', 'Defense', 'Passing', 'Tackle', 'Speed'];
 let myChart3 = document.getElementById("myChart3").getContext('2d');
-
 let chart3 = new Chart(myChart3, {
     type: 'radar',
     data: {
@@ -88,14 +79,10 @@ let chart3 = new Chart(myChart3, {
     }
 });
 
-
-
 let labels4 = ['Germany', 'France', 'UK', 'Italy', 'Spain', 'Others(23)'];
 let data4 = [83, 67, 66, 61, 47, 187];
 let colors4 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF', '#AC5353', '#CFD4D8'];
-
 let myChart4 = document.getElementById("myChart4").getContext('2d');
-
 let chart4 = new Chart(myChart4, {
     type: 'pie',
     data: {
