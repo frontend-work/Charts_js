3aae2913ed5f5d336aab0bb0469e2f383b91f1f0
Luminita Robciuc <88823221+frontend-work@users.noreply.github.com>
2021-12-05 16:33:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let labels1 = ['YES', 'YES BUT IN GREEN'];
 let data1 = [69, 31];
 let colors1 = ['#49A9EA', '#36CAAB'];
@@ -22,7 +21,7 @@ let chart1 = new Chart(myDoughnutChart, {
 
 let labels2 = ['American Airlines Group', 'Ryanair', 'China Southern Airlines', 'Lufthansa Group'];
 let data2 = [199.6, 130.3, 126.3, 130];
-let colors2 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF'];
+let colors2 = ['#49A9EA', '#36CAAB', '#ffbb33', '#ac7339'];
 let myChart2 = document.getElementById("myChart2").getContext('2d');
 let chart2 = new Chart(myChart2, {
     type: 'bar',
@@ -35,7 +34,7 @@ let chart2 = new Chart(myChart2, {
     },
     options: {
         title: {
-            text: "Number of passengers carried in 2017 (in mio.)",
+            text: "Number of passengers carried in 2020",
             display: true
         },
         legend: {
@@ -54,19 +53,19 @@ let chart3 = new Chart(myChart3, {
           {
             label: 'Messi',
             fill: true,
-            backgroundColor: "rgba(179, 181, 198, 0.2)",
-            borderColor: "#36CAAB",
+            backgroundColor: "rgba(0, 191, 255, 0.2)",
+            borderColor: "rgba(0, 191, 255, 1)",
             pointBorderColor: "#fff",
-            pointBackgroundColor: "#36CAAB",
+            pointBackgroundColor: "rgba(0, 191, 255, 1)",
             data: [50, 12, 55, 7, 29]
           },
           {
             label: 'Ronaldo',
             fill: true,
-            backgroundColor: "#49A9EA",
-            borderColor: "#49A9EA",
+            backgroundColor: "rgba(84, 171, 128, 0.2)",
+            borderColor: "rgba(84, 171, 128, 1)",
             pointBorderColor: "#fff",
-            pointBackgroundColor: "#49A9EA",
+            pointBackgroundColor: "rgba(84, 171, 128, 1)",
             data: [51, 10, 32, 20, 44]
           }
         ]
@@ -81,7 +80,7 @@ let chart3 = new Chart(myChart3, {
 
 let labels4 = ['Germany', 'France', 'UK', 'Italy', 'Spain', 'Others(23)'];
 let data4 = [83, 67, 66, 61, 47, 187];
-let colors4 = ['#49A9EA', '#36CAAB', '#34495E', '#B370CF', '#AC5353', '#CFD4D8'];
+let colors4 = ['#49A9EA', '#36CAAB', '#34495E', '#ffbb33', '#ac7339', '#CFD4D8'];
 let myChart4 = document.getElementById("myChart4").getContext('2d');
 let chart4 = new Chart(myChart4, {
     type: 'pie',
@@ -94,7 +93,7 @@ let chart4 = new Chart(myChart4, {
     },
     options: {
         title: {
-            text: "Population of the European Union (in mio)",
+            text: "Population of the European Union",
             display: true
         }
     }
